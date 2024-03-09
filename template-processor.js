8e852c887fafbb9132b945ff7c1be149f4959636
quanmv1 <quanquyhop14@gmail.com>
2024-03-09 16:59:40 +0700

    project 3

---
+++ template-processor.js
@@ -0,0 +1,22 @@
+function TemplateProcessor(template) {
+    this.template = template;
+}
+
+TemplateProcessor.prototype.fillIn = function (dictionary) {
+    var res = this.template;
+    var re = /{{[^{]*}}/g;
+    var match = this.template.match(re);
+    var pre, key, after;
+    for (var i = 0; i < match.length; i++) {
+        pre = match[i];
+        key = pre.replace("{{", "");
+        key = key.replace("}}", "");
+        after = dictionary[key] || '';
+        // if (after === undefined) {
+        // 	after = '';
+        // }
+
+        res = res.replace(pre, after);
+    }
+    return res;
+}
